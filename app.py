94865d41eee75fb6eb29c04e9f0392f991acc486
kamachiki <kamadachikiri@gmail.com>
2024-09-18 12:22:29 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,278 @@
+from chalice import Chalice, NotFoundError,BadRequestError, ConflictError,UnauthorizedError
+from chalicelib import database
+from datetime import datetime
+from urllib.parse import unquote
+from functools import reduce
+
+
+app = Chalice(app_name='hobopy-backend')
+app.debug = True
+app.api.cors = True
+
+# loginする
+@app.route('/login', methods=['POST'], cors=True)
+def login_user():
+    changes = app.current_request.json_body
+    user =database.get_user( changes["id"] )
+
+    if user==None:
+        return False
+
+    if user["Apassword"]==changes["Apassword"]:
+        session = database.create_session(user)
+        return session
+    else:
+        return False
+
+#/session/20250101のように表記する77
+@app.route('/session/{before_date}',methods=['DELETE'],cors=True)
+def session(before_date):
+    ses = _login_check()
+    post = app.current_request.json_body
+    user = ses['user']
+    if user == None or user["Atype"] != "admin":
+        raise BadRequestError(f"bad user")
+    return database.cleanup_session(datetime.strptime(before_date, '%Y%m%d'))
+
+#ログインを検査する
+def _login_check():
+    if "Authorization" not in app.current_request.headers :
+        raise UnauthorizedError("no bearer 1")
+    bearer = app.current_request.headers["Authorization"].split(" ")
+    if len(bearer) != 2:
+        raise UnauthorizedError("no bearer 2")
+    session_id = bearer[1]
+    check = database.session_check(session_id)
+    if check == None:
+        raise UnauthorizedError(f"no session")
+    user = database.get_user(check["Auser_id"])
+    if user == None:
+        raise UnauthorizedError(f"bad user")
+    return {"user":user,"session":check}
+
+def _checkKodomoToOtona(child_id,adult_id):
+    kodomoOtona = database.get_kodomoToOtona(child_id)
+    return reduce(lambda result, record: result or record["Aadult_id"] == adult_id, kodomoOtona, False)
+
+
+#ユーザーを作る、IDはかぶってはいけない
+@app.route('/users', methods=['POST'], cors=True)
+def create_user():
+    user = app.current_request.json_body
+        
+    ans = database.create_user(user)
+    if ans==None:
+        raise ConflictError("id is conflict")
+    else:
+        return ans
+
+#気分のデータを作成する
+@app.route('/kibuns', methods=['POST'], cors=True)
+def create_kibun():
+    kibun = app.current_request.json_body
+    session = _login_check()   
+    return database.create_kibun(kibun,session["user"]["id"])
+
+#大人が子供の気分のデータをとってくる、または、公開されている気分のデータをとる
+#mae:20240301
+#ushiro:20240401
+@app.route('/kibuns/{child_id}/{mae}/{ushiro}',methods=['GET'],cors=True)
+def get_kibuns(child_id,mae,ushiro):
+    child_id = unquote(child_id)
+    session = _login_check()
+    kokai = True
+    if child_id == session['user']["id"] or (session['user']['Atype'] == 'adult' and _checkKodomoToOtona(child_id,session["user"]["id"])):
+        kokai = False
+    return database.get_kibuns(child_id,datetime.strptime(mae, '%Y%m%d'),datetime.strptime(ushiro, '%Y%m%d'),kokai)
+       
+#きぶんを消す
+@app.route('/comments/{timestamp}', methods=['DELETE'])
+def delete_comments(timestamp):
+    session = _login_check()
+    return database.delete_comments(session["user"]["id"],timestamp)
+
+#大人と子供のデータを作成する
+@app.route('/kodomotootonas', methods=['POST'], cors=True)
+def create_kodomotootona():
+    kodomotootona = app.current_request.json_body
+    session = _login_check()   
+    return database.create_kodomotootona(kodomotootona,session["user"]["id"])
+
+#大人と子供のデータをとる
+@app.route('/kodomotootonas',methods=['GET'],cors=True)
+def get_kodomotootonas():
+    session = _login_check()
+    return database.get_kodomoToOtona(session["user"]["id"])
+   
+
+#大人と子供のデータを消す
+@app.route('/kodomotootonas/{adult_id}', methods=['DELETE'],cors=True)
+def delete_user(adult_id):
+    session = _login_check()
+    adult_id = unquote(adult_id)
+    return database.delete_kodomoToOtona(session["user"]["id"],adult_id)
+
+#よびかけのデータを作成する
+@app.route('/yobikakes', methods=['POST'], cors=True)
+def create_yobikake():
+    kibun = app.current_request.json_body
+    session = _login_check()   
+    return database.create_yobikake(kibun,session["user"]["id"])
+
+#子供への呼びかけのデータをとる
+#mae:20240301
+#ushiro:20240401
+@app.route('/yobikakes/{mae}/{ushiro}',methods=['GET'],cors=True)
+def get_yobikakes(mae,ushiro):
+    session = _login_check()
+    return database.get_yobikakes(session["user"]["id"],datetime.strptime(mae, '%Y%m%d'),datetime.strptime(ushiro, '%Y%m%d'))
+
+#よびかけを消す
+@app.route('/yobikake/{child_id}/{timestamp}', methods=['DELETE'], cors=True)
+def delete_yobikakes(child_id,timestamp):
+    child_id = unquote(child_id)
+    session = _login_check()
+    if session['user']['Atype'] != 'adult':
+        raise BadRequestError("adult only")
+    return database.delete_yobikakes(child_id,timestamp)
+
+#目標のデータを作成する
+@app.route('/goals', methods=['POST'], cors=True)
+def create_goal():
+    session = _login_check()
+    return database.create_goal(app.current_request.json_body,session["user"]["id"])
+
+#自分の目標のデータをとる
+#mae:20240301
+#ushiro:20240401
+@app.route('/goals/{mae}/{ushiro}',methods=['GET'],cors=True)
+def get_goals(mae,ushiro):
+    session = _login_check()
+    return database.get_goals(session["user"]["id"],datetime.strptime(mae, '%Y%m%d'),datetime.strptime(ushiro, '%Y%m%d'))
+#大人が子供の目標のデータをとる
+#mae:20240301
+#ushiro:20240401
+@app.route('/goals/{child_id}/{mae}/{ushiro}',methods=['GET'],cors=True)
+def get_goals(child_id,mae,ushiro):
+    
+    child_id = unquote(child_id)
+    session = _login_check()
+    if session['user']['Atype'] == 'adult' and _checkKodomoToOtona(child_id,session["user"]["id"]):
+        return database.get_kibuns(child_id,datetime.strptime(mae, '%Y%m%d'),datetime.strptime(ushiro, '%Y%m%d'),False)
+    else:
+        return []
+
+@app.route('/goals/{timestamp}', methods=['DELETE'], cors=True)
+def delete_goals(timestamp):
+    session = _login_check()
+    return database.delete_goals(session["user"]['id'],timestamp)
+
+#コメントのデータを作成する
+@app.route('/comments', methods=['POST'], cors=True)
+def create_comment():
+    session = _login_check()   
+    return database.create_comment(app.current_request.json_body,session["user"])
+
+#自分へのコメントのデータをとる
+#mae:20240301
+#ushiro:20240401
+@app.route('/comments/{mae}/{ushiro}',methods=['GET'],cors=True)
+def get_comments(mae,ushiro):
+    session = _login_check()
+    return database.get_comments(session["user"]["id"],datetime.strptime(mae, '%Y%m%d'),datetime.strptime(ushiro, '%Y%m%d'))
+
+#大人が子供へのコメントのデータをとる
+#mae:20240301
+#ushiro:20240401
+@app.route('/comments/{child_id}/{mae}/{ushiro}',methods=['GET'],cors=True)
+def get_comments(mae,ushiro):
+    child_id = unquote(child_id)
+    session = _login_check()
+    if session['user']['Atype'] != 'adult':
+        raise BadRequestError("adult only")
+    return database.get_comments(session["user"]["id"],datetime.strptime(mae, '%Y%m%d'),datetime.strptime(ushiro, '%Y%m%d'))
+
+#コメントを消す
+@app.route('/comments/{child_id}/{timestamp}', methods=['DELETE'], cors=True)
+def delete_comments(child_id,timestamp):
+    child_id = unquote(child_id)
+    session = _login_check()
+    if session['user']['Atype'] != 'adult':
+        raise BadRequestError("adult only")
+    return database.delete_comments(child_id,timestamp)
+
+#すべてのユーザーをリストアップする
+@app.route('/users', methods=['GET'], cors=True)
+def get_all_users():
+    session = _login_check()
+
+    users = database.get_all_users()
+    if users==None:
+        return False
+    for y in users:
+        if "Apassword" in y:
+            del y["Apassword"]
+    return users
+
+
+#すべてのおとなをリストアップする
+@app.route('/otona_users', methods=['GET'], cors=True)
+def get_otona_users():
+    return _get_users("adult")
+#すべての子供をリストアップする
+@app.route('/kodomo_users', methods=['GET'], cors=True)
+def get_kodomo_users():
+    return _get_users("child")
+#すべての人をリストアップする
+def _get_users(type):
+    session = _login_check()
+    users = database.get_users(type)
+    if users==None:
+        return False
+    for y in users:
+        if "Apassword" in y:
+            del y["Apassword"]
+    return users
+
+@app.route("/echo", methods=['GET'])
+def echo():
+    return {"echo":"echo"}
+
+#ユーザーをとってくる（公開対象になっている大人のみ
+@app.route('/kodomo/{user_id}', methods=['GET'], cors=True)
+def get_kodomo(user_id):
+    child_id = unquote(user_id)
+    session = _login_check()
+    if session['user']['Atype'] != 'adult':
+        raise BadRequestError("adult only")
+    kodomoOtona = database.get_kodomoToOtona(child_id)
+    if reduce(lambda result, record: result or record["Aadult_id"] == session["user"]["id"], kodomoOtona, False):
+        raise BadRequestError("kodomoToOtona List only")
+    user =  database.get_user(child_id)
+    if user==None:
+        return False
+    del user["Apassword"] 
+    return user
+
+#自分のユーザーをとってくる
+@app.route('/user', methods=['GET'], cors=True)
+def get_user():
+    session = _login_check()
+    user=session["user"]
+    del user["Apassword"] 
+    return user
+
+#自分のユーザー情報を変更する
+@app.route('/user', methods=['PUT'], cors=True)
+def update_user():
+    session = _login_check()
+    changes = app.current_request.json_body
+    return database.update_user(session["user"], changes)
+
+#ユーザーを消去する（大人のみ）
+#app.route('/users/{user_id}', methods=['DELETE'])
+#def delete_user(user_id):
+    #session = _login_check()
+    #if session['user']['Atype'] != 'adult':
+    #    raise BadRequestError("adult only")
+    #return database.delete_user(user_id)
---
+++ chalicelib/database.py
@@ -0,0 +1,323 @@
+import os
+import boto3
+import uuid
+import boto3.dynamodb
+from boto3.dynamodb.conditions import Key, Attr
+from datetime import *
+from decimal import *
+import html
+
+import boto3.dynamodb.conditions
+
+def _get_database(): #データーベースをとってくる
+    endpoint = os.environ.get('DB_ENDPOINT')
+    if endpoint:
+        return boto3.resource('dynamodb', endpoint_url=endpoint)
+    else:
+        return boto3.resource('dynamodb')
+    
+def get_user(user_id):
+    table = _get_database().Table(os.environ['DB_TABLE_USER'])
+    response = table.query(
+        KeyConditionExpression=Key('id').eq(user_id)
+    )
+    items = response['Items']
+    return items[0] if items else None
+
+def create_session(user):
+    item = {
+        "id":  uuid.uuid4().hex,
+        "Auser_id": user["id"],
+        "Atimestamp": Decimal(str(datetime.now().timestamp()))
+    }
+    table = _get_database().Table(os.environ['DB_TABLE_USER_SESSION'])
+    table.put_item(Item=item)
+    return item
+
+ # 対象の日時を指定 (before)
+def cleanup_session(before):
+    table = _get_database().Table(os.environ['DB_TABLE_USER_SESSION'])
+   
+    # スキャンして古いデータを列挙
+    response = table.scan(
+        FilterExpression='Atimestamp < :target_date',  # 作成日時が対象日時より前のデータをフィルタ
+        ExpressionAttributeValues={
+            ':target_date': Decimal( str(before.timestamp()) ) # Unixタイムスタンプに変換
+        }
+    )
+
+    # 削除対象のアイテムをリストに格納
+    items_to_delete = [{'id': item['id']} for item in response['Items']]  
+    # データが多い場合は、LastEvaluatedKeyを使って続きを取得
+    while 'LastEvaluatedKey' in response:
+        response = table.scan(
+            ExclusiveStartKey=response['LastEvaluatedKey'],
+            FilterExpression='Atimestamp < :target_date',
+            ExpressionAttributeValues={
+                ':target_date': before.timestamp()
+            }
+        )
+        items_to_delete.extend([{'id': item['id']} for item in response['Items']])
+    # items_to_delete の中身を確認
+    if items_to_delete:  # 削除対象がある場合のみ実行
+        # バッチ書き込みでアイテムを削除
+        with table.batch_writer() as batch:
+            for item in items_to_delete:
+                batch.delete_item( Key={
+                    'id': item['id']
+                })
+            return len(items_to_delete)
+    else:
+        return 0
+
+def session_check(session_id):
+    return _get_item(session_id,'DB_TABLE_USER_SESSION')
+
+def _get_item(id,tablename):
+    table = _get_database().Table(os.environ[tablename])
+    response = table.query(
+        KeyConditionExpression=Key('id').eq(id)
+    )
+    items = response['Items']
+    return items[0] if items else None
+
+def create_user(user):
+    auser = get_user(user["id"])
+    if auser==None:
+        if user['Atype']=="child":
+            
+            item = {
+            'id':user['id'],
+            'Apassword': user['Apassword'],
+            'Agakunen': user['Agakunen'],
+            'Atype': "child",
+        }
+        if user['Atype']=="adult":
+            item = {
+                'id':user['id'],
+                'Apassword':user['Apassword'],
+                'Amail-address':user['Amail-address'],
+                'Atype':"adult"
+            }
+        table = _get_database().Table(os.environ['DB_TABLE_USER'])
+        table.put_item(Item=item)
+        return item
+    else:
+        return None
+
+
+def _get_lists(query_name,user_id,mae,ushiro,tablename):
+    table = _get_database().Table(os.environ[tablename])
+
+    keyp =Key(query_name).eq(user_id) & Key('Atimestamp').between(Decimal(str(mae.timestamp())), Decimal(str(ushiro.timestamp())))
+
+    # スキャンして期間内のデータを列挙
+    response = table.query(KeyConditionExpression = keyp)
+    items = response['Items']
+    # データが多い場合は、LastEvaluatedKeyを使って続きを取得
+    while 'LastEvaluatedKey' in response:
+        response = table.query(
+            ExclusiveStartKey=response['LastEvaluatedKey'],
+            KeyConditionExpression = keyp
+            )
+        items.extend(response['Items'])
+    return items
+
+#よびかけの記録をつくる
+def create_yobikake(yobikake_data,user_id):
+    item ={
+        'Atimestamp':Decimal(str(datetime.now().timestamp())),
+        'Ayobikakeru':user_id,
+        'Ayobikakerareru':yobikake_data["Ayobikakerareru"],
+        'Akibun_timestamp':yobikake_data['Akibun_timestamp'],
+        'Ayobikake':html.escape(yobikake_data['Ayobikake']),
+        'Akokai':yobikake_data['Akokai']
+    }
+    table = _get_database().Table(os.environ['DB_TABLE_YOBIKAKE'])
+    table.put_item(Item=item)
+    return item 
+
+#気分の記録をつくる
+def create_kibun(kibun,child_id):
+    item ={
+        'Atimestamp':Decimal(str(datetime.now().timestamp())),
+        'Aface':kibun['Aface'],
+        'Atype':kibun['Atype'],
+        'Areason':html.escape(kibun['Areason']),
+        'Akokai':bool(kibun['Akokai']),
+        'Achild_id':child_id
+
+    }
+    table = _get_database().Table(os.environ['DB_TABLE_KIBUN'])
+    table.put_item(Item=item)
+    return item   
+
+#goalの記録をつくる
+def create_goal(goal,child_id):
+    item ={
+        'Atimestamp':Decimal(str(datetime.now().timestamp())),
+        'Achild_id':child_id,
+        'Agoal':html.escape(goal['Agoal'])
+    }
+    table = _get_database().Table(os.environ['DB_TABLE_GOAL'])
+    table.put_item(Item=item)
+    return item   
+#commentの記録をつくる
+def create_comment(comment,adult_id):
+    item ={
+        'Atimestamp':Decimal(str(datetime.now().timestamp())),
+        'Achild_id':comment['Achild_id'],
+        'Aadult_id':adult_id,
+        'Acomment':html.escape(comment['Acomment'])
+    }
+    table = _get_database().Table(os.environ['DB_TABLE_GOAL'])
+    table.put_item(Item=item)
+    return item   
+
+#子供と大人の記録を作る
+def create_kodomotootona(kodomotootona,child_id):
+    item ={
+        'Achild_id':child_id,
+        'Aadult_id':kodomotootona['Aadult_id']
+    }
+    table = _get_database().Table(os.environ['DB_TABLE_KodomoToOtona'])
+    table.put_item(Item=item)
+    return item
+
+#子供と大人の記録を消す
+def delete_kodomoToOtona(child_id,adult_id):
+    table = _get_database().Table(os.environ['DB_TABLE_KodomoToOtona'])
+    response = table.delete_item(
+
+        Key={
+            "Achild_id":child_id,
+            "Aadult_id":adult_id
+        },
+        ReturnValues='ALL_OLD'
+    )
+    if 'Attributes' in response:
+        return response['Attributes']
+    else:
+        return False
+
+#子供のIDから大人を取り出す
+def get_kodomoToOtona(child_id):
+    table=_get_database().Table(os.environ['DB_TABLE_KodomoToOtona'])
+    response = table.query(
+        KeyConditionExpression=Key('Achild_id').eq(child_id)
+    )
+    items = response['Items']
+    return items
+
+
+#maeからushiroの間の気分をとりだす。それぞれdatetime型
+def get_kibuns(child_id,mae,ushiro,kokai):
+    table = _get_database().Table(os.environ['DB_TABLE_KIBUN'])
+    # スキャンして期間内のデータを列挙
+    keyp =Key('Achild_id').eq(child_id) & Key('Atimestamp').between(Decimal(str(mae.timestamp())), Decimal(str(ushiro.timestamp())))
+
+    if kokai:
+        response = table.query(
+            KeyConditionExpression = keyp,
+            FilterExpression=Attr('Akokai').eq(True)
+        )
+    else:
+        response = table.query(KeyConditionExpression =keyp) 
+    items = response['Items']
+    return items
+
+#maeからushiroの間の気分をとりだす。それぞれdatetime型
+def get_yobikakes(child_id,mae,ushiro):
+    return _get_lists('Ayobikakerareru',child_id,mae,ushiro,'DB_TABLE_YOBIKAKE')
+def get_goals(child_id,mae,ushiro):
+    return _get_lists('Achild_id',child_id,mae,ushiro,'DB_TABLE_GOAL')
+def get_comments(child_id,mae,ushiro):
+    return _get_lists('Achild_id',child_id,mae,ushiro,'DB_TABLE_COMMENT')
+
+#項目を消す
+def delete_kibuns(child_id,timestamp):
+    return _delete_item("Achild_id",child_id,"Atimestamp",timestamp,'DB_TABLE_KIBUN')
+def delete_yobikakes(child_id,timestamp):
+    return _delete_item('Achild_id',child_id,"Atimestamp",timestamp,'DB_TABLE_YOBIKAKE')
+def delete_goals(child_id,timestamp):
+    return _delete_item('Achild_id',child_id,"Atimestamp",timestamp,'DB_TABLE_GOAL')
+def delete_comments(child_id,timestamp):
+    return _delete_item('Achild_id',child_id,"Atimestamp",timestamp,'DB_TABLE_COMMENT')
+
+def _delete_item(id_name,id,timestamp_name,timestamp,table_name):
+    table = _get_database().Table(os.environ[table_name])
+    result = table.delete_item(
+        Key={
+            id_name:id,
+            timestamp_name:timestamp
+        },
+        ReturnValues='ALL_OLD'
+    )
+    return result['Attributes']
+
+def get_all_users():
+    table = _get_database().Table(os.environ['DB_TABLE_USER'])
+    response = table.scan()
+    return response['Items']
+
+def get_otona_users():
+    return get_users("adult")
+
+def get_kodomo_users():
+    return get_users("child")
+
+def get_users(type):
+    table = _get_database().Table(os.environ['DB_TABLE_USER'])
+    response = table.scan(
+        FilterExpression =Attr("Atype").eq(type)
+    )
+    return response['Items']
+
+
+def get_user(user_id):
+    table=_get_database().Table(os.environ['DB_TABLE_USER'])
+    response = table.query(
+        KeyConditionExpression=Key('id').eq(user_id)
+    )
+    items = response['Items']
+    return items[0] if items else None
+
+# user_id = 124522
+# changes = { "id":"hogehgoe", "name": "かまだちきり", "password":"aaaaa", "type":"child","gakunen":"5"}
+# changes = { "id":"hogehgoe", "name": "かまだちきり", "password":"aaaaa", "type":"adult","mail":"aaa@aaa.jp"}
+def update_user(user_id, changes):
+    table = _get_database().Table(os.environ['DB_TABLE_USER'])
+
+    update_expression = []
+    expression_attribute_values = {}
+    if (changes["Atype"]=="child"):
+        keys = ['id','Aname','Apassword','Atype','Agakunen']
+    elif (changes["type"]=="adult"):
+        keys = ['id','Aname','Apassword','Atype','Amail-address']
+
+    for key in keys :
+        if key in changes:
+            update_expression.append(f"{key} = :{key[0:2]}")
+            expression_attribute_values[f":{key[0:2]}"] = changes[key]
+
+    result = table.update_item(
+        Key={
+            'id': user_id,
+        },
+        UpdateExpression='set ' + ','.join(update_expression),
+        ExpressionAttributeValues=expression_attribute_values,
+        ReturnValues='ALL_NEW'
+
+    )
+    return result['Attributes']
+
+
+def delete_user(user_id):
+    table = _get_database().Table(os.environ['DB_TABLE_USER'])
+    result = table.delete_item(
+        Key={
+            'id':user_id,
+        },
+        ReturnValues='ALL_OLD'
+    )
+    return result['Attributes']
