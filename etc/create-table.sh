404fd371c939fd653aa4760b8619c288f0fd029b
k <k@kenonotobukkukonpyuta.local>
2024-09-19 11:49:21 +0900

    aws upload success

---
+++ etc/create-table.sh
@@ -0,0 +1,7 @@
+aws dynamodb create-table --cli-input-json file://schema_comment.json		
+aws dynamodb create-table --cli-input-json file://schema_user.json
+aws dynamodb create-table --cli-input-json file://schema_user_session.json
+aws dynamodb create-table --cli-input-json file://schema_goal.json
+aws dynamodb create-table --cli-input-json file://schema_yobikake.json
+aws dynamodb create-table --cli-input-json file://schema_kibun.json
+aws dynamodb create-table --cli-input-json file://schema_kodomotootona.json
