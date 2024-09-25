a4ca8b32c89f2580a0d338f9eb8b1b74fe6e29bc
kamachiki <kamadachikiri@gmail.com>
2024-09-25 21:05:04 +0900

    database

--- test/test.py
+++ test/test.py
@@ -1,6 +1,21 @@
 import boto3
 import csv
 
+from boto3.dynamodb.conditions import Key, Attr
+import datetime
+from decimal import *
+
+
+dynamodb = boto3.resource('dynamodb', endpoint_url="http://127.0.0.1:8001")
+
+def _changeTimestamp(keyname,data):
+    if keyname in data:
+        data[keyname + "_norm"] = datetime.datetime.fromtimestamp(float(data[keyname])).strftime("%Y%m%d%H%M")
+    return data
+
+def _changeListTimestamp(keyname,datalist):
+    return list(map(lambda x: _changeTimestamp(keyname,x), datalist))
+
 def save_dynamodb_table_to_csv(table_name, csv_file_name):
     """
     指定されたDynamoDBテーブルのデータをCSVファイルに保存する関数
@@ -10,10 +25,7 @@ def save_dynamodb_table_to_csv(table_name, csv_file_name):
         csv_file_name (str): 保存するCSVファイル名
     """
 
-    dynamodb = boto3.resource('dynamodb', endpoint_url="http://127.0.0.1:8001")
-    #dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(table_name)
-
     # テーブル全体のスキャンを実行
     response = table.scan()
     data = response['Items']
@@ -22,6 +34,11 @@ def save_dynamodb_table_to_csv(table_name, csv_file_name):
     while 'LastEvaluatedKey' in response:
         response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
         data.extend(response['Items'])
+    data = _changeListTimestamp("Atimestamp",data)
+
+    write_csv(csv_file_name,data,table_name)
+
+def write_csv(csv_file_name,data,table_name):
 
     # CSVファイルに書き込み
     with open(csv_file_name, 'w', newline='', encoding='utf-8') as csvfile:
@@ -38,4 +55,14 @@ def save_dynamodb_table_to_csv(table_name, csv_file_name):
 
 save_dynamodb_table_to_csv("Yobikake","yobikake.csv")
 save_dynamodb_table_to_csv("Users","user.csv")
+
+save_dynamodb_table_to_csv("Kibun","kibun.csv")
+table = dynamodb.Table("Yobikake")
+keyp =Key("Ayobikakerareru").eq("ちいちゃん") #& Key('Atimestamp').between(Decimal(str(1726827119.491559)), Decimal(str(1726927119.491559)))
+
+# スキャンして期間内のデータを列挙
+response = table.query(KeyConditionExpression = keyp)
+data = response['Items']
+write_csv("yobikake_between.csv",data,"Yobikake")
+
 print("全て終了")
